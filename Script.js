9aa4cb2ec5e0e33834ea34fa2675aada1a6502cd
Richard-L <rrlaw93@gmail.com>
2024-02-29 10:45:28 +0000

    InProgress

---
+++ Script.js
@@ -0,0 +1,7 @@
+document.getElementById("menuButton").addEventListener("mouseover", function() {
+    document.getElementById("hiddenMenu").style.display = "block";
+  });
+  
+  document.getElementById("menuButton").addEventListener("mouseout", function() {
+    document.getElementById("hiddenMenu").style.display = "none";
+  });
